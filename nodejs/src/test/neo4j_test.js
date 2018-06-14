bcea02a5cff369ea3659175dbaece25afe4b34e6
cyj <ycheng4@e.ntu.edu.sg>
2018-06-14 22:31:22 +0800

    uncomment neo4j_test which was commented previously and forgotten about

--- nodejs/src/test/neo4j_test.js
+++ nodejs/src/test/neo4j_test.js
@@ -1,34 +1,34 @@
-// var expect = require('chai').expect;
-// var neo4jHelper = require('../neo4jHelper.js');
+var expect = require('chai').expect;
+var neo4jHelper = require('../neo4jHelper.js');
 
-// describe('neo4jFunc Test', function () {
-// 	before(function (done) {
-// 		// run incase after case failed
-// 		neo4jHelper.dropDb(function (err, result) {
-// 			if (err) return err;
-// 			done();
-// 		});
-// 	});
+describe('neo4jFunc Test', function () {
+	before(function (done) {
+		// run incase after case failed
+		neo4jHelper.dropDb(function (err, result) {
+			if (err) return err;
+			done();
+		});
+	});
 	
-// 	describe('findRelation()', function () {
-// 		var userEmail = "youjun89@gmail.com";
-// 		var followerEmail = "youjun9@gmail.com";
+	describe('findRelation()', function () {
+		var userEmail = "youjun89@gmail.com";
+		var followerEmail = "youjun9@gmail.com";
 
-// 		before(function (done) {
-// 			// run incase after case failed
-// 			neo4jHelper.createRelation(function (err, node) {
-// 				done();
-// 			}, userEmail, followerEmail)
-// 		});
+		before(function (done) {
+			// run incase after case failed
+			neo4jHelper.createRelation(function (err, node) {
+				done();
+			}, userEmail, followerEmail)
+		});
 
-// 		it('find relation between 2 nodes exist', function (done) {
-// 			neo4jHelper.findRelation(function (err, records) {
-// 				if (err) {
-// 					return err;
-// 				};
-// 				expect(records).to.have.length.above(0);
-// 				done();
-// 			}, userEmail, followerEmail)
-// 		});
-// 	});
-// });
+		it('find relation between 2 nodes exist', function (done) {
+			neo4jHelper.findRelation(function (err, records) {
+				if (err) {
+					return err;
+				};
+				expect(records).to.have.length.above(0);
+				done();
+			}, userEmail, followerEmail)
+		});
+	});
+});
