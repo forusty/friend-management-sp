5b5602413151ecea33491d5c814d49ce3d4e1ec1
cyj <ycheng4@e.ntu.edu.sg>
2018-06-14 22:32:29 +0800

    added swagger ui for api documentation and handling of all known routes or root route to /api-docs

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -3,8 +3,17 @@ var express = require('express');
 var app = express();
 var neo4jHelper = require('./neo4jHelper.js');
 var bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // loading of swagger docs
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/', function (req, res, next) {
+    res.redirect('/api-docs');
+});
 
 app.get('/add', function (req, res, next) {
     var node = {
@@ -187,8 +196,12 @@ app.get('/tools/drop', function (req, res, next) {
     });
 });
 
+app.use(function(req, res, next){
+    res.redirect('/api-docs');
+  });
+
+
 app.use(function (err, req, res, next) {
-    console.log(err);
     res.status(err.status || 500);
     res.json({ "success": false, "message": "Uh-oh, Something terrible has went wrong!", "code": 1 });
 });
